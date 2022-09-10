5e9a50fc7bb298e2e91f2f3cb9e5e43a76c750ab
tree70373 <tree70373@gmail.com>
2022-09-10 14:50:51 +0800

    test branch

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get("/restaurants/:id", (req, res) => {
 });
 
 // 設定路由: GET編輯餐廳頁
-app.get("/restaurants/:id/edit", (req, res) => {
+app.put("/restaurants/:id", (req, res) => {
   const id = req.params.id;
   Restaurant.findById(id)
     .lean()
